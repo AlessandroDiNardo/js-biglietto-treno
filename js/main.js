2f9d9c85d4a2b923cb8b6629e418a326c094786d
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-07 16:51:19 +0100

    Esercizio completo

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/* chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65. */
+
+const costoBiglietto = 0.21;
+
+//chiedere numero di km
+let km = parseFloat(prompt("Inserisci quanti km vuoi percorrere:"));
+
+console.log(km);
+
+//chiedere età
+let eta = parseInt(prompt("inserisci la tua età:"));
+console.log(eta);
+
+
+// calcolo perzzo biglietto per intero
+prezzo = costoBiglietto * km;
+console.log(prezzo);
+
+//applicazione sconto
+let tot;
+ 
+// sconto per minorenni
+if(eta > 18 && eta < 65){
+    tot =  prezzo;
+}
+
+if(eta < 18){
+    tot = (prezzo / 100) * 20;
+}
+
+// sconto per over 65
+if(eta > 65){
+    tot = (prezzo / 100) * 40;
+}
+
+console.log(tot);
+
+
+//output
+document.getElementById("trip").innerHTML = `${tot}€`;
